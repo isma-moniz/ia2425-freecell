b833e99e55c4c339dabba8a14116dc7054f558d0
xicccp <franciscofernandes23@gmail.com>
2025-04-03 09:05:48 +0100

    changed main temporarily until gui is fixed

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 # main.py
 from freecell_gui import FreeCellGUI
+from freecell_game import FreeCell
+
 
 if __name__ == "__main__":
-    game_gui = FreeCellGUI()
-    game_gui.run()
+    mode = input("Select mode (human/bot/gui): ").strip().lower()
+
+    game = FreeCell()
+
+    if mode == "human":
+        game.play_human()
+    elif mode == "bot":
+        game.play_bot()
+    
+    elif mode == "gui":
+        game_gui = FreeCellGUI()
+        game_gui.run()
+    else:
+        print("Invalid mode selected!")
